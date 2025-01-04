72c1229ed9bf644da61e0399bfac05db4eb74475
MCEG\F1686533 <f1686533@email.foxconn.com>
2025-01-04 16:34:49 +0800

    feat: 优化 camera 组件 & size 组件

--- src/js/camera.js
+++ src/js/camera.js
@@ -4,24 +4,40 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Experience from './experience.js';
 
 export default class Camera {
-  constructor() {
+  constructor(orthographic = false) {
     this.experience = new Experience();
     this.sizes = this.experience.sizes;
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
+    this.orthographic = orthographic;
 
     this.setInstance();
     this.setControls();
   }
 
   setInstance() {
-    this.instance = new THREE.PerspectiveCamera(
-      35,
-      this.sizes.width / this.sizes.height,
-      0.1,
-      100
-    );
-    this.instance.position.set(6, 4, 8);
+    if (this.orthographic) {
+      const aspect = this.sizes.aspect;
+      this.frustumSize = 15;
+
+      this.instance = new THREE.OrthographicCamera(
+        -this.frustumSize * aspect,
+        this.frustumSize * aspect,
+        this.frustumSize,
+        -this.frustumSize,
+        -1,
+        100
+      );
+    } else {
+      this.instance = new THREE.PerspectiveCamera(
+        35,
+        this.sizes.width / this.sizes.height,
+        0.1,
+        100
+      );
+    }
+    this.instance.position.set(-10, 10, 10);
+    this.instance.lookAt(new THREE.Vector3());
     this.scene.add(this.instance);
   }
 
@@ -31,8 +47,19 @@ export default class Camera {
   }
 
   resize() {
-    this.instance.aspect = this.sizes.width / this.sizes.height;
-    this.instance.updateProjectionMatrix();
+    if (this.orthographic) {
+      const aspect = this.sizes.width / this.sizes.height;
+
+      this.instance.left = (-this.frustumSize * aspect) / 2;
+      this.instance.right = (this.frustumSize * aspect) / 2;
+      this.instance.top = this.frustumSize / 2;
+      this.instance.bottom = -this.frustumSize / 2;
+
+      this.instance.updateProjectionMatrix();
+    } else {
+      this.instance.aspect = this.sizes.width / this.sizes.height;
+      this.instance.updateProjectionMatrix();
+    }
   }
 
   update() {
--- src/js/utils/sizes.js
+++ src/js/utils/sizes.js
@@ -8,6 +8,7 @@ export default class Sizes extends EventEmitter {
     // Setup
     this.width = window.innerWidth;
     this.height = window.innerHeight;
+    this.aspect = this.width / this.height;
     this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
     // Resize event
