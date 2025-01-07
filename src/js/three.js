8aa643907356080542702d0c6242cc82836748e4
MCEG\F1686533 <f1686533@email.foxconn.com>
2025-01-07 15:26:47 +0800

    feat: 新增 TSL 模板

--- src/js/three.js
+++ src/js/three.js
@@ -1,10 +1,20 @@
-import * as THREE from 'three';
-// eslint-disable-next-line import/no-unresolved
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-import fragment from '../shaders/fragment.glsl';
-import vertex from '../shaders/vertex.glsl';
+import {
+  color,
+  Fn,
+  fog,
+  positionLocal,
+  rangeFogFactor,
+  sin,
+  time,
+  uniform,
+  uv,
+  vec2,
+  vec3,
+  vec4
+} from 'three/tsl';
+import * as THREE from 'three/webgpu';
 
 const device = {
   width: window.innerWidth,
@@ -27,13 +37,10 @@ export default class Three {
     this.camera.position.set(2, 2, 2);
     this.scene.add(this.camera);
 
-    this.renderer = new THREE.WebGLRenderer({
+    this.renderer = new THREE.WebGPURenderer({
       canvas: this.canvas,
-      alpha: true,
-      antialias: true,
-      preserveDrawingBuffer: true
+      forceWebGL: false
     });
-    this.renderer.outputColorSpace = THREE.SRGBColorSpace;
     this.renderer.setSize(device.width, device.height);
     this.renderer.setPixelRatio(Math.min(device.pixelRatio, 2));
 
@@ -44,7 +51,6 @@ export default class Three {
     this.loader = new GLTFLoader();
 
     this.setLights();
-    this.setGeometry();
     this.setObject();
     this.render();
     this.setResize();
@@ -56,56 +62,20 @@ export default class Three {
   }
 
   setObject() {
-    const bakedTexture = new THREE.TextureLoader().load('./baked.png');
-    bakedTexture.flipY = false;
-    bakedTexture.colorSpace = THREE.SRGBColorSpace;
-    const bakedMaterial = new THREE.MeshBasicMaterial({
-      map: bakedTexture
-    });
+    this.material = new THREE.NodeMaterial();
+    this.material.colorNode = Fn(() => {
+      return vec4(1, 0, 0, 1);
+    })();
 
-    const poleLightMaterial = new THREE.MeshBasicMaterial({
-      color: '#fefefd'
-    });
-
-    this.loader.load('./threejsblend.glb', ({ scene }) => {
-      scene.children.find((child) => child.name === 'baked').material =
-        bakedMaterial;
-      this.scene.add(scene);
-      scene.children.find((child) => child.name === 'poleLightA').material =
-        poleLightMaterial;
-      this.scene.add(scene);
-      scene.children.find((child) => child.name === 'poleLightB').material =
-        poleLightMaterial;
-      scene.children.find((child) => child.name === 'portalLight').material =
-        poleLightMaterial;
-      this.scene.add(scene);
-    });
-  }
-
-  setGeometry() {
-    this.planeGeometry = new THREE.PlaneGeometry(1, 1, 128, 128);
-    this.planeMaterial = new THREE.ShaderMaterial({
-      side: THREE.DoubleSide,
-      wireframe: true,
-      fragmentShader: fragment,
-      vertexShader: vertex,
-      uniforms: {
-        progress: { type: 'f', value: 0 }
-      }
-    });
-
-    this.planeMesh = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
-    this.planeMesh.visible = false;
+    this.planeGeometry = new THREE.PlaneGeometry(2, 2);
+    this.planeMesh = new THREE.Mesh(this.planeGeometry, this.material);
     this.scene.add(this.planeMesh);
   }
 
   render() {
     const elapsedTime = this.clock.getElapsedTime();
 
-    this.planeMesh.rotation.x = 0.2 * elapsedTime;
-    this.planeMesh.rotation.y = 0.1 * elapsedTime;
-
-    this.renderer.render(this.scene, this.camera);
+    this.renderer.renderAsync(this.scene, this.camera);
     requestAnimationFrame(this.render.bind(this));
   }
 
