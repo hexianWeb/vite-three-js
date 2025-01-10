08230dcd84e29837cd18c7cc6d6505123c735617
MCEG\F1686533 <f1686533@email.foxconn.com>
2025-01-10 15:51:29 +0800

    feat: 优化 IMouse 调用

--- src/js/experience.js
+++ src/js/experience.js
@@ -4,6 +4,7 @@ import Camera from './camera.js';
 import Renderer from './renderer.js';
 import sources from './sources.js';
 import Debug from './utils/debug.js';
+import IMouse from './utils/imouse.js';
 import Resources from './Utils/resources.js';
 import Sizes from './utils/sizes.js';
 import Stats from './utils/stats.js';
@@ -33,10 +34,11 @@ export default class Experience {
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
-    this.camera = new Camera(true);
+    this.camera = new Camera();
     this.renderer = new Renderer();
     this.resources = new Resources(sources);
     this.physics = new PhysicsWorld();
+    this.iMouse = new IMouse();
     this.world = new World();
 
     this.sizes.on('resize', () => {
@@ -54,8 +56,9 @@ export default class Experience {
 
   update() {
     this.camera.update();
-    this.renderer.update();
     this.world.update();
+    this.renderer.update();
     this.stats.update();
+    this.iMouse.update();
   }
 }
