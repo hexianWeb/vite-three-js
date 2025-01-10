0e7fff63d7e8469c42411086027d78f553018a67
MCEG\F1686533 <f1686533@email.foxconn.com>
2025-01-10 15:43:46 +0800

    feat: 新增 camera 调控 Pane

--- src/js/camera.js
+++ src/js/camera.js
@@ -10,9 +10,15 @@ export default class Camera {
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
     this.orthographic = orthographic;
+    this.debug = this.experience.debug;
+    this.debugActive = this.experience.debug.active;
+
+    this.position = new THREE.Vector3(4.79, 3.4, 17);
+    this.target = new THREE.Vector3(3.3, 3.4, -1.4);
 
     this.setInstance();
     this.setControls();
+    this.setDebug();
   }
 
   setInstance() {
@@ -30,20 +36,49 @@ export default class Camera {
       );
     } else {
       this.instance = new THREE.PerspectiveCamera(
-        35,
+        34,
         this.sizes.width / this.sizes.height,
         0.1,
         100
       );
     }
-    this.instance.position.set(-10, 10, 10);
-    this.instance.lookAt(new THREE.Vector3());
+    this.instance.position.copy(this.position);
+    this.instance.lookAt(this.target);
     this.scene.add(this.instance);
   }
 
   setControls() {
     this.controls = new OrbitControls(this.instance, this.canvas);
     this.controls.enableDamping = true;
+    this.controls.target.copy(this.target);
+  }
+
+  setDebug() {
+    if (this.debugActive) {
+      const cameraFolder = this.debug.ui.addFolder({
+        title: 'Camera',
+        expanded: false
+      });
+
+      cameraFolder
+        .addBinding(this, 'position', {
+          label: 'camera Position'
+        })
+        .on('change', this.updateCamera.bind(this));
+
+      cameraFolder
+        .addBinding(this, 'target', {
+          label: 'camera Target'
+        })
+        .on('change', this.updateCamera.bind(this));
+    }
+  }
+
+  updateCamera() {
+    this.instance.position.copy(this.position);
+    this.instance.lookAt(this.target);
+    this.controls.target.copy(this.target);
+    this.controls.update();
   }
 
   resize() {
