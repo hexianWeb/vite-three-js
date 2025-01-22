97bc0df7261dbe9527a497285e7a7a3139008c19
MCEG\F1686533 <f1686533@email.foxconn.com>
2025-01-22 09:01:53 +0800

    feat: 修改 Marker 定位原理

--- src/js/world/earthMarker.js
+++ src/js/world/earthMarker.js
@@ -17,9 +17,24 @@ export default class EarthMarker {
     // 标记数组
     this.markers = [
       { title: '北京', coords: { lat: 39.9042, lng: 116.4074 } },
-      { title: '纽约', coords: { lat: 40.7128, lng: -74.006 } }
+      { title: '纽约', coords: { lat: 40.7128, lng: -74.006 } },
+      {
+        title: '台湾',
+        coords: {
+          lat: 24.915_71,
+          lng: 121.6739
+        }
+      }
       // 可以添加更多标记
     ];
+
+    // 创建位置辅助器
+    this.lonHelper = new THREE.Object3D();
+    this.latHelper = new THREE.Object3D();
+    this.lonHelper.add(this.latHelper);
+    this.positionHelper = new THREE.Object3D();
+    this.positionHelper.position.z = this.earthRadius;
+    this.latHelper.add(this.positionHelper);
 
     this.createMarkers();
 
@@ -27,36 +42,31 @@ export default class EarthMarker {
     this.scene.add(this.markersGroup);
   }
 
-  // 经纬度转换为三维坐标
-  latLngToVector3(lat, lng) {
-    const phi = (90 - lat) * (Math.PI / 180);
-    const theta = (lng + 180) * (Math.PI / 180);
-
-    const x = -this.earthRadius * Math.sin(phi) * Math.cos(theta);
-    const y = this.earthRadius * Math.cos(phi);
-    const z = this.earthRadius * Math.sin(phi) * Math.sin(theta);
-
-    return new THREE.Vector3(x, y, z);
-  }
-
   createMarkers() {
-    // const markerGeometry = new THREE.SphereGeometry(0.02, 16, 16); // 小球体几何体
-    const markerGeometry = new THREE.BoxGeometry(0.02, 0.5, 0.02); // 小球体几何体
-    const markerMaterial = new THREE.MeshBasicMaterial({ color: 0xFF_00_00 }); // 红色材质
+    const markerGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.5);
+    markerGeometry.applyMatrix4(
+      new THREE.Matrix4().makeTranslation(0, 0, 0.25)
+    );
+
+    const markerMaterial = new THREE.MeshBasicMaterial({ color: 0xFF_00_00 });
 
     for (const marker of this.markers) {
-      const position = this.latLngToVector3(
-        marker.coords.lat,
-        marker.coords.lng
-      );
-      const markerMesh = new THREE.Mesh(markerGeometry, markerMaterial);
-      markerMesh.position.copy(position);
+      const mesh = new THREE.Mesh(markerGeometry, markerMaterial);
+
+      // 调整辅助器指向经纬度
+      this.lonHelper.rotation.y =
+        THREE.MathUtils.degToRad(marker.coords.lng) + Math.PI * 0.5;
+      this.latHelper.rotation.x = THREE.MathUtils.degToRad(-marker.coords.lat);
+
+      // 更新世界矩阵
+      this.positionHelper.updateWorldMatrix(true, false);
+      mesh.applyMatrix4(this.positionHelper.matrixWorld);
 
       // 将标记的标题存储在userData中
-      markerMesh.userData.title = marker.title;
+      mesh.userData.title = marker.title;
 
-      // 将标记添加到组中，而不是直接添加到场景
-      this.markersGroup.add(markerMesh);
+      // 将标记添加到组中
+      this.markersGroup.add(mesh);
     }
   }
 
